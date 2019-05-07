760451ebda871407b5bafdcd043200014da9b374
Dwij Sukesh Kumar Sheth <dwijhariket@gmail.com>
2019-05-07 10:20:00 +0530

    Added some Basic Routes

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+module.exports = function (app) {
+    app.get('/', function (req, res) {
+        res.redirect('./home');
+    });
+    app.use('/home', require('./home'));
+    app.use('/signup', require('./signup'));
+    app.use('/signin', require('./signin'));
+    app.use('/signout', require('./signout'));
+    app.use('/inventory', require('./inventory'));
+    app.use('/search', require('./search'));
+    app.use('/profile', require('./profile'));
+    app.use('/borrow', require('./borrow'));
+    app.use('/quick_borrow', require('./quick_borrow'));
+    app.use('/approval', require('./approval'));
+    // 404 page
+    app.use(function (req, res) {
+        if (!res.headersSent) {
+            res.status(404).render('404');
+        }
+    });
+};
